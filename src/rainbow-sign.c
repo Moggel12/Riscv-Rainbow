45414ae4ef0bf746002cf210120fa54511135bc3
Mikkel-MBP <juulvestergaard@hotmail.com>
2021-03-20 16:22:53 +0100

    Initial porting of signing

--- src/rainbow-sign.c
+++ src/rainbow-sign.c
@@ -14,49 +14,62 @@
 
 
 int main( int argc , char ** argv )
-{
-	printf( "%s\n", CRYPTO_ALGNAME );
+{	
+	// printf( "%s\n", CRYPTO_ALGNAME );
 
-        printf("sk size: %lu\n", CRYPTO_SECRETKEYBYTES );
-        printf("pk size: %lu\n",  CRYPTO_PUBLICKEYBYTES );
-        printf("hash size: %d\n", _HASH_LEN );
-        printf("signature size: %d\n\n", CRYPTO_BYTES );
+	// printf("sk size: %lu\n", CRYPTO_SECRETKEYBYTES );
+	// printf("pk size: %lu\n",  CRYPTO_PUBLICKEYBYTES );
+	// printf("hash size: %d\n", _HASH_LEN );
+	// printf("signature size: %d\n\n", CRYPTO_BYTES );
 
-	if( !(3 == argc) ) {
-		printf("Usage:\n\n\trainbow-sign sk_file_name file_to_be_signed\n\n");
-		return -1;
-	}
+	send_start();
+	send_unsigned("sk size: %lu\n", CRYPTO_SECRETKEYBYTES, 10);
+	send_unsigned("pk size: %lu\n",  CRYPTO_PUBLICKEYBYTES, 10);
+	send_unsigned("hash size: %d\n", _HASH_LEN, 10);
+	send_unsigned("signature size: %d\n\n", CRYPTO_BYTES, 10);
 
-	uint8_t *_sk = (uint8_t*)malloc( CRYPTO_SECRETKEYBYTES );
+	// if( !(3 == argc) ) {
+	// 	printf("Usage:\n\n\trainbow-sign sk_file_name file_to_be_signed\n\n");
+	// 	return -1;
+	// }
+
+	//uint8_t *_sk = (uint8_t*)malloc( CRYPTO_SECRETKEYBYTES );
+	uint8_t _sk[CRYPTO_SECRETKEYBYTES];
 
 	FILE * fp;
 	int r = 0;
 
-	fp = fopen( argv[1] , "r");
-	if( NULL == fp ) {
-		printf("fail to open secret key file.\n");
-		return -1;
-	}
-	r = byte_fget( fp ,  _sk , CRYPTO_SECRETKEYBYTES );
-	fclose( fp );
-	if( CRYPTO_SECRETKEYBYTES != r ) {
-		printf("fail to load key file.\n");
-		return -1;
-	}
+	// fp = fopen( argv[1] , "r");
+	// if( NULL == fp ) {
+	// 	printf("fail to open secret key file.\n");
+	// 	return -1;
+	// }
 
-	unsigned char * msg = NULL;
-	unsigned long long mlen = 0;
-	r = byte_read_file( &msg , &mlen , argv[2] );
-	if( 0 != r ) {
-		printf("fail to read message file.\n");
-		return -1;
-	}
+	/*
+	 * Put code to get the key here
+	 */
 
-	unsigned char * signature = malloc( mlen + CRYPTO_BYTES );
-	if( NULL == signature ) {
-		printf("alloc memory for signature buffer fail.\n");
-		return -1;
-	}
+	// r = byte_fget( fp ,  _sk , CRYPTO_SECRETKEYBYTES );
+	// fclose( fp );
+	// if( CRYPTO_SECRETKEYBYTES != r ) {
+	// 	printf("fail to load key file.\n");
+	// 	return -1;
+	// }
+
+	/*
+	 * Put code to obtain message to be signed
+	 */
+
+	// unsigned char * msg = NULL;
+	// unsigned long long mlen = 0;
+	// r = byte_read_file( &msg , &mlen , argv[2] );
+	// if( 0 != r ) {
+	// 	printf("fail to read message file.\n");
+	// 	return -1;
+	// }
+
+	// unsigned char * signature = malloc( mlen + CRYPTO_BYTES );
+	unsigned char signature[mlen + CRYPTO_BYTES];
 
 	unsigned long long smlen = 0;
 	r = crypto_sign( signature, &smlen, msg , mlen , _sk );
@@ -65,12 +78,16 @@ int main( int argc , char ** argv )
 		return -1;
 	}
 
-	byte_fdump( stdout , CRYPTO_ALGNAME " signature"  , signature + mlen , CRYPTO_BYTES );
-	printf("\n");
+	// byte_fdump( stdout , CRYPTO_ALGNAME " signature"  , signature + mlen , CRYPTO_BYTES );
+	/*
+	 * Output signed message as above
+	 */
 
-	free( msg );
-	free( signature );
-	free( _sk );
+	// printf("\n");
+
+	// free( msg );
+	// free( signature );
+	// free( _sk );
 
 	return 0;
 }
